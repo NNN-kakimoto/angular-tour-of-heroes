16513515247fd2c71390ceb7bb5265e5344e0ed3
NNN <b7080@oic.jp>
2018-06-11 19:20:25 +0900

    make hero editor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+	hero: Hero = {
+		id: 1,
+		name: 'Windstorm'
+	};
 
   constructor() { }
 
   ngOnInit() {
-  }
+	}
 
 }
